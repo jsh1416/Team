d4ae1d3af071814403c27979335bc3c8ed794fde
rainbon0 <rainbon0@naver.com>
2021-06-09 14:57:33 +0900

    6.9 2:57 bong

--- group1/src/main/java/com/itbank/controller/MemberController.java
+++ group1/src/main/java/com/itbank/controller/MemberController.java
@@ -1,6 +1,7 @@
 package com.itbank.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 //06.09 bong 
@@ -9,5 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/member")
 public class MemberController {
 
+	@GetMapping("/login")
+	public String login() {
+		return "/member/login";
+	}
 	
+	@GetMapping("/join")
+	public String join() {
+		return "/member/join";
+	}
 }
