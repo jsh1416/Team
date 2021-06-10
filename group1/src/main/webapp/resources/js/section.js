745cdf0cbadf70201dc75f19106848f5d1e60228
jsh1416 <wkdtjdgh1191@naver.com>
2021-06-10 10:42:36 +0900

    스크롤 적용

---
+++ group1/src/main/webapp/resources/js/section.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  
+  // fullpage customization
+  $('#fullpage').fullpage({
+    sectionSelector: '.section',
+    fixedElements : '.fix',
+    navigation: true,
+    slidesNavigation: true,
+    css3: true,
+    controlArrows: false    
+  }); 
+  
+});
