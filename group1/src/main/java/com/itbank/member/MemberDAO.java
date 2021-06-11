ccda541f2939b59df9e07ddd755e9bdc4b1cc119
rainbon0 <rainbon0@naver.com>
2021-06-11 15:09:02 +0900

    06.11 봉찬균 수정 메인컬러 수정 및 이메일,아이디,비밀번호 정규식 및 일치여부 확인 기능

--- group1/src/main/java/com/itbank/member/MemberDAO.java
+++ group1/src/main/java/com/itbank/member/MemberDAO.java
@@ -4,6 +4,7 @@ package com.itbank.member;
 import org.apache.ibatis.annotations.Select;
 
 	// 06. 10 봉찬균 MemberDAO 생성
+//
 public interface MemberDAO {
 
 	@Select("select * from EPLmember where id=#{id} and pw = #{pw}")
